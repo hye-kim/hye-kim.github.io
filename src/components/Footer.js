995a3428d8701e84584d518ff79e81045ab177a5
hye <davidckim22@gmail.com>
2021-04-19 22:50:18 -0400

    small fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div id="footer" className="flex items-centera py-2 sm:py-4">
+    <div id="footer" className="flex items-center py-2 sm:py-4">
       <div className="pl-16">
         <a href="https://www.linkedin.com/in/hye-kim/">
           <i className="fab fa-linkedin-in text-white text-4xl sm:text-5xl"></i>
